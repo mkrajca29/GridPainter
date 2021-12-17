32041f615ed6a307de8bc26a5dfde507ff283aa8
unknown <mkrajca29@gmail.com>
2021-12-17 14:09:10 +0100

    deploy - demo data

---
+++ precache-manifest.24caaf4ea2ca4131a3f1d2d9844bbf69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc27a1500e3186dbf92",
+    "url": "/GridPainter/css/app.3db103ba.css"
+  },
+  {
+    "revision": "99c1e2c0f2684b02882d1da828ee38ca",
+    "url": "/GridPainter/img/gear.99c1e2c0.svg"
+  },
+  {
+    "revision": "87095a1e6cf4c998aad61ef65f789074",
+    "url": "/GridPainter/index.html"
+  },
+  {
+    "revision": "3fc27a1500e3186dbf92",
+    "url": "/GridPainter/js/app.fb7eae56.js"
+  },
+  {
+    "revision": "b70fe22635d993c6bc13",
+    "url": "/GridPainter/js/chunk-vendors.d0cbb372.js"
+  },
+  {
+    "revision": "00b2ddc995515a08d741af01df9b3074",
+    "url": "/GridPainter/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/GridPainter/robots.txt"
+  }
+]);
